09ed63417662bd2cace47986bbf73a9555790914
Mauricio van der Maesen de Sombreff <mauriciovandermaesendesombreff@li-ma.nl>
2022-05-21 22:39:39 +0200

    random?csv=1

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -66,7 +66,53 @@ router.get('/random', (req, res) => {
         const r = Math.floor(Math.random()*count);
         const drawing = result[r];
         delete drawing._id;
-        res.status(200).json(drawing);
+        if(req.query.csv){
+
+            const fields = [
+            'ts', 'x', 'y', 
+            'dt', 'dx', 'dy', 
+            'pressure',
+             // 'red', 'green', 'blue', 
+             'pen_down', 'pen_up'];
+            const opts = { fields };
+
+            strokes = drawing.strokes.filter(stroke=>stroke.points.length > 0).map(
+                stroke => {
+                  x = stroke.points[0].x;
+                  y = stroke.points[0].y;
+                  t = stroke.points[0].timestamp;
+                  let num = stroke.points.length-1;
+                  return stroke.points.map( 
+                    (point, n) => {
+                      return {
+                        ts: point.timestamp,    // timestamp of point
+                        x: point.x,            // absolute coordinates on canvas
+                        y: point.y,            // absolute coordinates on canvas
+                        dt: point.timestamp-t,  // delta time since last point
+                        dx: point.x-x,          // horizontal distance from last point 
+                        dy: point.y-y,          // vertical distance from last point
+                        pressure: point.pressure,     // point pressure
+                        //red: stroke.color[0],    // Red
+                        //green: stroke.color[1],    // Green
+                        //blue: stroke.color[2],    // Blue
+                        pen_down: n===0?1:0,          // stroke start
+                        pen_up: n===num?1:0,        // stroke end
+                      };                    
+                    }
+                  )
+                }
+              ).flat();
+
+            const csv = parse(strokes, opts);
+            console.log(csv);
+            res.type('text/csv');
+            res.status(200).send(csv);
+          }
+          else {
+
+
+          res.status(200).json(drawing);
+        }
       }
     });
 });
