576d9637a7b91bcd68c6da268e5fd7900dff8f86
n2048-creative-technology <56191120+n2048-creative-technology@users.noreply.github.com>
2022-06-13 22:54:02 +0200

    Add files via upload

---
+++ predictor/README.md
@@ -0,0 +1,3 @@
+echo [1,2,3,4,5,6,7,8,9,10] | docker run -a stdin -a stdout -i -t predictor > out.txt
+
+
---
+++ predictor/api.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+class Simplify(Resource):
+    def post(self, data):
+        result = {}
+        return jsonify(result)
+
+class Expand(Resource):
+    def post(self, data):
+        result = {}
+        return jsonify(result)
+
+class Predict(Resource):
+    def posr(self, data):
+        result = {}
+        return jsonify(result)
+        
+api.add_resource(Simplify, '/simplify') # Route_1
+api.add_resource(Expand, '/expand') # Route_2
+api.add_resource(Predict, '/predict') # Route_3
+
+if __name__ == '__main__':
+     app.run(port='5002')
+     
